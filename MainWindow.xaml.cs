131cc28d79aa9c95cd0e7d79c899b96c11ba0c83
JesseStanley <47399968+JesseStanley@users.noreply.github.com>
2019-03-26 12:53:33 -0400

    added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-/*Jesse Stanley
-* 3/26/2019
-* text message translater
-*/
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -32,17 +28,29 @@ namespace _184784TXTMSG
         {
             string TextEntered = txtTextbox.Text;
             TextEntered = TextEntered.Replace("CU", "see you");
+            TextEntered = TextEntered.Replace("see you", "CU");
             TextEntered = TextEntered.Replace(":-)", "I'm happy");
+            TextEntered = TextEntered.Replace("I'm happy", ":-)");
             TextEntered = TextEntered.Replace(":-(", "I'm unhappy");
+            TextEntered = TextEntered.Replace("I'm unhappy", ":-(");
             TextEntered = TextEntered.Replace(";-)", "wink");
+            TextEntered = TextEntered.Replace("wink", ";-)");
             TextEntered = TextEntered.Replace(":-P", "stick out tongue");
+            TextEntered = TextEntered.Replace("stick out tongue", ":-P");
             TextEntered = TextEntered.Replace("(˜.˜)", "sleepy");
+            TextEntered = TextEntered.Replace("sleepy", "(˜.˜)");
             TextEntered = TextEntered.Replace("TA", "totally awesome");
+            TextEntered = TextEntered.Replace("totally awesome", "TA");
             TextEntered = TextEntered.Replace("CCC", "Canadian Computing Competition");
+            TextEntered = TextEntered.Replace("Canadian Computing Competition", "CCC");
             TextEntered = TextEntered.Replace("CUZ", "because");
+            TextEntered = TextEntered.Replace("because", "CUZ");
             TextEntered = TextEntered.Replace("TY", "thank-you");
+            TextEntered = TextEntered.Replace("thank-you", "TY");
             TextEntered = TextEntered.Replace("YW", "you're welcome");
+            TextEntered = TextEntered.Replace("you're welcome", "YW");
             TextEntered = TextEntered.Replace("TTYL", "talk to you later");
+            TextEntered = TextEntered.Replace("talk to you later", "TTYL");
             lblLabel.Content = TextEntered;
         }
     }
